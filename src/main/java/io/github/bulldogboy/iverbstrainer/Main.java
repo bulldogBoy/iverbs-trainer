51050ba0798102495933c9e3bbb4f02ef38d6366
bulldogBoy <yarik_kosmi4eskiy@proton.me>
2026-04-15 13:04:45 +0300

    Added launch call in main

--- src/main/java/io/github/bulldogboy/iverbstrainer/Main.java
+++ src/main/java/io/github/bulldogboy/iverbstrainer/Main.java
@@ -1,7 +1,12 @@
 package io.github.bulldogboy.iverbstrainer;
 
-public class Main {
-	public static void main(String[] args) {
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		DataOperations db = new DataOperations();
+		VerbsTrainerService<Verb> service = new VerbsTrainerService<>(new ArrayList<Verb>(db.getAllVerbs()));
+		service.run();
 	}
 }
