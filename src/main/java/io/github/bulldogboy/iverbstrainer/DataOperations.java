37d5860f70e610ad86992b7d8dee09b77378cffc
bulldogBoy <yarik_kosmi4eskiy@proton.me>
2026-04-15 12:52:49 +0300

    In DataOperations class was added getAllVerbs() as List method

--- src/main/java/io/github/bulldogboy/iverbstrainer/DataOperations.java
+++ src/main/java/io/github/bulldogboy/iverbstrainer/DataOperations.java
@@ -1,6 +1,8 @@
 package io.github.bulldogboy.iverbstrainer;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataOperations {
 	private final String connectionString = "jdbc:sqlite:sqlite.db";
@@ -16,5 +18,24 @@ public class DataOperations {
 		} catch (SQLException e) {
 			System.out.println("Error with connection to DB");
 		}
+	}
+
+	public List<Verb> getAllVerbs()  {
+		List<Verb> verbs = new ArrayList<>();
+	try {
+		String sqlQuerry = "SELECT * FROM irregural_verbs";
+		try (Statement statement = con.createStatement(); ResultSet resultSet = statement.executeQuery(sqlQuerry)) {
+			while (resultSet.next()) {
+				Verb verb = new Verb(resultSet.getInt("id"), resultSet.getString("infinitive"),
+						resultSet.getString("past_simple"), resultSet.getString("past_participle"),
+						resultSet.getString("translation"), resultSet.getString("datagroup"));
+				verbs.add(verb);
+
+			}
+		}
+	} catch (SQLException e) {
+		System.out.println("Failed to retrieve data from DB");
+	}
+	return verbs;
 	}
 }
